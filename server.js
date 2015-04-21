b36b7549216faa46df066566701ca83077439175
Chris Patterson <chris@pattersonplace.org>
2015-04-21 16:36:58 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var server = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('<html><head></head><body>Hello World</body></html>');
   // Prevents the build container from timing out by waiting for the server to close.
+  // added a nothing change
   process.nextTick(function () {
     console.log('Stopping server running at http://127.0.0.1:1337');
     process.exit();
